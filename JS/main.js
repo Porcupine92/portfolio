89acffc9f85a0247afbe515211e3bc3d83b93c41
Porcupine92 <szymonjozefowicz1992@gmail.com>
2020-09-28 23:36:47 +0200

    working on responsive menu

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-
+    menu();
 });
+
+const menu = () => {
+    $('.burger-menu').on('click', () => {
+        const state = $('.navigation').hasClass("navigation--open");
+        if (state) {
+            $('.navigation').removeClass('navigation--open');
+        } else {
+            $('.navigation').addClass('navigation--open');
+        }
+    })
+}
